b8c7153115ce2e74deb82f6dd091758d9402b85e
Hastari Utama <utama738@gmail.com>
2023-06-20 09:18:24 +0700

    auth

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RakBukuController;
+use App\Http\Controllers\LoginRegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -41,3 +42,13 @@ Route::get('/buku', function () {
 });
 // Rak Buku
 Route::resource('rak_buku', RakBukuController::class);
+
+// Register & Login
+Route::controller(LoginRegisterController::class)->group(function() {
+    Route::get('/register', 'register')->name('register');
+    Route::post('/store', 'store')->name('store');
+    Route::get('/login', 'login')->name('login');
+    Route::post('/authenticate', 'authenticate')->name('authenticate');
+    Route::get('/dashboard', 'dashboard')->name('dashboard');
+    Route::post('/logout', 'logout')->name('logout');
+});
